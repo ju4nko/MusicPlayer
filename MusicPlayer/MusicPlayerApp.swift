284acd671f7dc009ff4be3a6426194b29e3d4546
ju4nko <ju4nko@gmail.com>
2026-06-08 16:17:57 +0200

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Juanjo on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
