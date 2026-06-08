55275f7277f519337906a8e2d902e5dd2dd20783
ju4nko <ju4nko@gmail.com>
2026-06-08 16:38:31 +0200

    Creado el modelo Song con la estructura de las propiedades de una canción

---
+++ MusicPlayer/Models/Song.swift
@@ -0,0 +1,18 @@
+//
+//  Song.swift
+//  MusicPlayer
+//
+//  Created by Juanjo on 08/06/2026.
+//
+import Foundation
+
+struct Song: Identifiable, Hashable {
+    
+    let id: UUID = UUID()
+    let title: String
+    let url: URL
+    let duration: TimeInterval
+    let artist: String?
+    let artwork: URL?
+    
+}
